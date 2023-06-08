2cb38200cc339973f0003fb0fd8910a7f2a8dbad
Adam Osusky <adam.osusky02@gmail.com>
2023-06-08 14:43:58 +0200

    Login, logout and creation of account

--- ranker_site/ranker_site/settings.py
+++ ranker_site/ranker_site/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ranker_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# login params
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'dashboard'
